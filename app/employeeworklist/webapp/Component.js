1361993b1fe781b2fa1da6c883490b35dff82811
Shubham Kumar <kumar.shubham8869@gmail.com>
2024-03-15 16:15:48 +0000

    Employee Worklist Added

---
+++ app/employeeworklist/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.app.employeeworklist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
